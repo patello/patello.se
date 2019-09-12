ce89de73f77da1752d11ad242e115576ac958fb3
Patrik Ekenberg <Patrik@Ekenberg.com>
2019-09-12 18:39:52 +0000

    Revert "Renamed path after migration"
    This reverts commit 7116344d191c435c134bb652f2c1e72fb5b2f476.

--- Docker/start_server.sh
+++ Docker/start_server.sh
@@ -1,2 +1,2 @@
- docker run --name patello.se -d -v ~/projects/patello.se/content:/usr/share/nginx/html:ro -p 80:80 patello.se
+ docker run --name patello.se-nginx -d -v /usr/local/src/patello.se/content:/usr/share/nginx/html:ro -p 80:80 patello.se
 
